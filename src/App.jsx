a2a01f48d189389df736013e36f0fd23cf814559
FELIPE <galindo@MacBook-Air-de-FELIPE.local>
2026-09-16 20:51:31 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useRef, useState } from "react";
+
+function App() {
+  const audioRef = useRef(null);
+
+  const [reproduciendo, setReproduciendo] = useState(false);
+  const [progreso, setProgreso] = useState(0);
+
+  const reproducirPausar = () => {
+    if (reproduciendo) {
+      audioRef.current.pause();
+      setReproduciendo(false);
+    } else {
+      audioRef.current.play();
+      setReproduciendo(true);
+    }
+  };
+
+  const actualizarProgreso = () => {
+    const audio = audioRef.current;
+
+    if (audio.duration) {
+      const porcentaje =
+        (audio.currentTime / audio.duration) * 100;
+
+      setProgreso(porcentaje);
+    }
+  };
+
+  return (
+    <main className="reproductor">
+
+      <section className="portada">
+        <img
+          src="/imgen.jpeg"
+          alt="Portada de la canción"
+        />
+      </section>
+
+      <section className="informacion">
+        <h1>DANIEEELAAAA</h1>
+        <p>Tee quieeroooooo</p>
+      </section>
+
+      <section className="progreso">
+        <div
+          className="barra"
+          style={{ width: `${progreso}%` }}
+        >
+          <div className="indicador"></div>
+        </div>
+      </section>
+
+      <section className="controles">
+
+        <button className="boton-control">
+          ⏮
+        </button>
+
+        <button
+          className="boton-play"
+          onClick={reproducirPausar}
+        >
+          {reproduciendo ? "❚❚" : "▶"}
+        </button>
+
+        <button className="boton-control">
+          ⏭
+        </button>
+
+      </section>
+
+      <audio
+        ref={audioRef}
+        src="/cancion.mp3"
+        onTimeUpdate={actualizarProgreso}
+      />
+
+    </main>
+  );
+}
+
+export default App;
